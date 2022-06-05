f2bd63783737eb58e311d7ecd36d476baaebc7d8
jerry <qaz013039@gmail.com>
2022-06-05 11:44:08 +0800

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2022, 6, 4) },
+    { title: "Toilet Paper", amount: 94.12, date: new Date(2022, 6, 3) },
+    { title: "New TV", amount: 299.49, date: new Date(2022, 6, 2) },
+    { title: "New Desk (Wooden)", amount: 450, date: new Date(2022, 6, 1) },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }

58c339decd0b6b3798a21eb889d94e321fdb0f80
jerry <qaz013039@gmail.com>
2022-06-07 22:22:23 +0800

    Working with Multiple States

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -13,7 +14,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
